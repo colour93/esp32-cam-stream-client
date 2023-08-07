c80014496508682ba41e148922d543b796aba3ce
colour93 <colour_93@furry.top>
2023-08-08 00:27:30 +0800

    📝 Verify Sign [50%]

---
+++ README.md
@@ -0,0 +1,3 @@
+# esp32-cam-stream-client
+
+边学边做（
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <esp_camera.h>
 #include <WiFi.h>
+#include <Crypto.h>
+#include <SHA256.h>
 #include <ESPAsyncWebServer.h>
 
 #include "config.h"
@@ -38,11 +40,39 @@ void DEBUG(Generic text)
   Serial.println(text);
 }
 
-void setup()
+boolean verifySign(String ts, String sign)
 {
-  Serial.begin(115200);
-  DEBUG("starting...");
 
+  String data = ts + SECRET;
+
+  SHA256 sha256;
+  sha256.update((const uint8_t *)data.c_str(), data.length());
+  uint8_t hash[32];
+  sha256.finalize(hash, 32);
+
+  String thisSign = "";
+  for (int i = 0; i < 32; i++)
+  {
+    if (hash[i] < 16)
+      thisSign += "0";
+    thisSign += String(hash[i], HEX);
+  }
+
+  DEBUG("This Sign: " + thisSign);
+
+  if (sign.equalsIgnoreCase(thisSign))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+// 连接 WiFi
+void connectWiFi()
+{
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   while (WiFi.status() != WL_CONNECTED)
@@ -54,9 +84,12 @@ void setup()
   DEBUG("Connected to WiFi");
   DEBUG("IP Address: ");
   DEBUG(WiFi.localIP());
+}
 
-  // 初始化摄像头
-  DEBUG("initializing camera...");
+// 初始化摄像头
+void initCamera()
+{
+  DEBUG("Initializing camera...");
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -79,7 +112,7 @@ void setup()
   config.xclk_freq_hz = xclk * 1000000;
   config.pixel_format = PIXFORMAT_JPEG;
   config.grab_mode = CAMERA_GRAB_LATEST;
-  // Pre-allocate large buffers
+
   if (psramFound())
   {
     config.frame_size = FRAMESIZE_UXGA;
@@ -97,31 +130,69 @@ void setup()
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK)
   {
-    DEBUG("camera initialization failed");
+    DEBUG("Camera initialization failed");
     return;
   }
   else
   {
-    DEBUG("camera initialized");
+    DEBUG("Camera initialized");
   }
+}
 
-  // 设置 Web 服务器路由
+// 初始化 web 服务端
+void initWebServer()
+{
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send(200, "text/plain", "Hello, ESP32!"); });
 
   server.on("/camera", HTTP_GET, [](AsyncWebServerRequest *request)
             {
-    camera_fb_t * fb = NULL;
-    fb = esp_camera_fb_get();
-    if (!fb) {
-      request->send(500, "text/plain", "camera capture failed");
-      return;
-    }
+    // 获取查询参数
+    if (request->hasParam("ts") && request->hasParam("sign"))
+    {
+      String ts = request->getParam("ts")->value();
+      String sign = request->getParam("sign")->value();
 
-    request->send_P(200, "image/jpeg", (const uint8_t*)fb->buf, fb->len);
-    esp_camera_fb_return(fb); });
+      // 处理查询参数
+      DEBUG("Timestamp: " + ts);
+      DEBUG("Sign: " + sign);
+
+      boolean verfied = verifySign(ts, sign);
+
+      if (verfied) {
+        
+        camera_fb_t * fb = NULL;
+        fb = esp_camera_fb_get();
+
+        if (!fb) {
+          request->send(500, "text/plain", "Camera capture failed");
+          return;
+        }
+
+        request->send_P(200, "image/jpeg", (const uint8_t*)fb->buf, fb->len);
+        esp_camera_fb_return(fb);
+
+      } else {
+        request -> send(403, "text/plain", "Forbidden");
+      }
+
+    }
+    else
+    {
+      request->send(400, "text/plain", "Bad Request");
+    } });
 
   server.begin();
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  DEBUG("starting...");
+
+  connectWiFi();
+  initCamera();
+  initWebServer();
 }
 
 void loop()
